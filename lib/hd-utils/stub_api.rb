2e84f99ad666ff6b1e3208947006bcb5722ef3da
apainintheneck <apainintheneck@gmail.com>
2023-08-20 12:25:29 -0700

    Remove unused helpers

--- lib/hd-utils/stub_api.rb
+++ lib/hd-utils/stub_api.rb
@@ -4,13 +4,5 @@ module HDUtils
   module StubAPI
     autoload :Formula, "hd-utils/stub-api/formula"
     autoload :Cask, "hd-utils/stub-api/cask"
-
-    def self.formula!
-      StubAPI::Formula
-    end
-
-    def self.cask!
-      StubAPI::Cask
-    end
   end
 end

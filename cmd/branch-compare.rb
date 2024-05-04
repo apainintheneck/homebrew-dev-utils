b2d4272aadca72d1f3b4a5f2a775c73af286dea5
apainintheneck <apainintheneck@gmail.com>
2024-05-04 11:03:36 -0700

    Update commands to new DSL
    TLDR: The old syntax still works but was deprecated in favor of
    this new command syntax.
    
    - https://github.com/Homebrew/brew/issues/16814
    - https://docs.brew.sh/External-Commands#external-commands-in-taps

--- cmd/branch-compare.rb
+++ cmd/branch-compare.rb
@@ -4,43 +4,44 @@ require "cli/parser"
 require_relative "../lib/hd_utils"
 
 module Homebrew
-  def self.branch_compare_args
-    Homebrew::CLI::Parser.new do
-      usage_banner "`branch-compare` [<options>] -- command"
-      description <<~EOS
-        Runs a brew command on both the current branch and the main branch
-        and then diffs the output of both commands. This helps with debugging
-        and assurance testing when making changes to important commands.
+  module Cmd
+    class BranchCompare < AbstractCommand
+      cmd_args do
+        usage_banner "`branch-compare` [<options>] -- command"
+        description <<~EOS
+          Runs a brew command on both the current branch and the main branch
+          and then diffs the output of both commands. This helps with debugging
+          and assurance testing when making changes to important commands.
 
-        Example: `brew branch-compare --quiet -- deps --installed`
+          Example: `brew branch-compare --quiet -- deps --installed`
 
-        #{HDUtils::BranchDiff::WARNING_MESSAGE}
-      EOS
+          #{HDUtils::BranchDiff::WARNING_MESSAGE}
+        EOS
 
-      switch "--ignore-errors", description: "Continue diff when a command returns a non-zero exit code."
-      switch "--with-stderr", description: "Combine stdout and stderr in diff output."
-      switch "--word-diff", description: "Show word diff instead of default line diff."
-      switch "--time", description: "Benchmark the command on both branches."
-      switch "--local", description: "Only load formula/cask from local taps not the API."
+        switch "--ignore-errors", description: "Continue diff when a command returns a non-zero exit code."
+        switch "--with-stderr", description: "Combine stdout and stderr in diff output."
+        switch "--word-diff", description: "Show word diff instead of default line diff."
+        switch "--time", description: "Benchmark the command on both branches."
+        switch "--local", description: "Only load formula/cask from local taps not the API."
 
-      named_args :command, min: 1
+        named_args :command, min: 1
+      end
+
+      def run
+        command = args.named
+
+        ENV.delete("HOMEBREW_INTERNAL_JSON_V3")
+
+        HDUtils::BranchDiff.diff_output(
+          command,
+          quiet:         args.quiet?,
+          word_diff:     args.word_diff?,
+          with_stderr:   args.with_stderr?,
+          ignore_errors: args.ignore_errors?,
+          no_api:        args.local?,
+          benchmark:     args.time?,
+        )
+      end
     end
-  end
-
-  def self.branch_compare
-    args = branch_compare_args.parse
-    command = args.named
-
-    ENV.delete("HOMEBREW_INTERNAL_JSON_V3")
-
-    HDUtils::BranchDiff.diff_output(
-      command,
-      quiet:         args.quiet?,
-      word_diff:     args.word_diff?,
-      with_stderr:   args.with_stderr?,
-      ignore_errors: args.ignore_errors?,
-      no_api:        args.local?,
-      benchmark:     args.time?,
-    )
   end
 end
--- cmd/service-diff.rb
+++ cmd/service-diff.rb
@@ -4,52 +4,52 @@ require "cli/parser"
 require_relative "../lib/hd_utils"
 
 module Homebrew
-  def self.service_diff_args
-    Homebrew::CLI::Parser.new do
-      usage_banner "`service-diff` [<options>]"
-      description <<~EOS
-        Compare the service file generation on macOS and Linux before
-        and after changes to brew. This helps with debugging and assurance
-        testing when making changes to the `brew services` DSL.
+  module Cmd
+    class ServiceDiff < AbstractCommand
+      cmd_args do
+        usage_banner "`service-diff` [<options>]"
+        description <<~EOS
+          Compare the service file generation on macOS and Linux before
+          and after changes to brew. This helps with debugging and assurance
+          testing when making changes to the `brew services` DSL.
 
-        #{HDUtils::BranchDiff::WARNING_MESSAGE}
-      EOS
+          #{HDUtils::BranchDiff::WARNING_MESSAGE}
+        EOS
 
-      flag "--formula=", description: "Run the diff on only one formula."
-      flag "--tap=", description: "Run the diff on only one tap."
-      switch "--word-diff", description: "Show word diff instead of default line diff."
-      switch "--stat", description: "Shows condensed output based on `git diff --stat`"
+        flag "--formula=", description: "Run the diff on only one formula."
+        flag "--tap=", description: "Run the diff on only one tap."
+        switch "--word-diff", description: "Show word diff instead of default line diff."
+        switch "--stat", description: "Shows condensed output based on `git diff --stat`"
 
-      conflicts "--tap=", "--formula="
-      conflicts "--word-diff", "--stat"
-    end
-  end
-
-  def self.service_diff
-    args = service_diff_args.parse
-
-    ENV.delete("HOMEBREW_INTERNAL_JSON_V3")
-
-    script_path = File.expand_path("../lib/diff-scripts/service_diff.rb", __dir__)
-    odie "Script #{script_path} doesn't exist!" unless File.exist?(script_path)
-
-    script_args =
-      if args.tap
-        ["tap", args.tap]
-      elsif args.formula
-        ["formula", args.formula]
-      else
-        ["all"]
+        conflicts "--tap=", "--formula="
+        conflicts "--word-diff", "--stat"
       end
 
-    command = [HOMEBREW_BREW_FILE, "ruby", "--", script_path, *script_args]
+      def run
+        ENV.delete("HOMEBREW_INTERNAL_JSON_V3")
 
-    HDUtils::BranchDiff.diff_directories(
-      command,
-      quiet:     args.quiet?,
-      word_diff: args.word_diff?,
-      stat:      args.stat?,
-      no_api:    true,
-    )
+        script_path = File.expand_path("../lib/diff-scripts/service_diff.rb", __dir__)
+        odie "Script #{script_path} doesn't exist!" unless File.exist?(script_path)
+
+        script_args =
+          if args.tap
+            ["tap", args.tap]
+          elsif args.formula
+            ["formula", args.formula]
+          else
+            ["all"]
+          end
+
+        command = [HOMEBREW_BREW_FILE, "ruby", "--", script_path, *script_args]
+
+        HDUtils::BranchDiff.diff_directories(
+          command,
+          quiet:     args.quiet?,
+          word_diff: args.word_diff?,
+          stat:      args.stat?,
+          no_api:    true,
+        )
+      end
+    end
   end
 end
